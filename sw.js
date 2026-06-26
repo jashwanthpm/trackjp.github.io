753a35841039f0d693a691da5404c760ecf1a2b7
jashwanthpm <110254036+jashwanthpm@users.noreply.github.com>
2026-06-27 00:11:31 +0530

    Update and rename sw-github-pages.js to sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // GITHUB PAGES OPTIMIZED SERVICE WORKER
 // GitHub Pages has some network restrictions, this version handles that
 
-const CACHE_NAME = 'sakkhi-github-v1.0';
+const CACHE_NAME = 'sakkhi-v9';
 
 const URLS_TO_CACHE = [
   '/',
